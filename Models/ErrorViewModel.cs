0227d3e583bb590c5dd6731b9e11e46b7678bbf1
tuskenincursor@gmail.com <tuskenincursor@gmail.com>
2023-04-23 00:29:27 -0300

    commit mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Parcial_Martin_Taborda.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
